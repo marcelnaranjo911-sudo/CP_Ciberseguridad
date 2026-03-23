6bfe83ba81939f2b76611456a71a1d4cbdc00809
Marcel360 <marcelnaranjo911@gmail.com>
2026-03-22 21:55:06 -0400

    Update auth_controller.py
    Ejercicio 2: Eliminado eval() y aplicada Whitelist de comandos

--- CP3_Ciberseguridad/app/controllers/auth_controller.py
+++ CP3_Ciberseguridad/app/controllers/auth_controller.py
@@ -3,11 +3,15 @@ from app.services.user_service import UserService
 class AuthController:
     def __init__(self):
         self.service = UserService()
+        
+        self.allowed_diagnostics = {
+            "check_status": "Sistema Operativo: Online",
+            "get_version": "Versión de la App: 1.1 (Security Patch applied)"
+        }
 
     def attempt_login(self, username, password):
-    
         return self.service.authenticate(username, password)
 
     def run_system_diagnostic(self, command):
-      
-        return eval(command)
+       
+        return self.allowed_diagnostics.get(command, "Error: Comando no autorizado o inexistente.")
